1448a9282487b3a0e2031ace943e7f47dcb5861b
kaushtubhkanishk <kaushtubh1@gmail.com>
2025-05-18 10:07:52 +0530

    Refactored the code for learning

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ from constants import *
 
 def main():
     pygame.init()
-
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    color = pygame.Color(0, 0, 0)
+    
+    pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
-        screen.fill(color)
-        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+        pygame.Surface.fill(surface, color=pygame.Color(255, 255, 255))
+
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
